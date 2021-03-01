060bd37a5d01243c6c595de60586a471ec53fd25
Lucas Ritossa <22.lucas.ritossa@westg.org>
2021-02-28 20:25:18 -0500

    spotify api functionality, need to fix array allocation

--- Handler/UrlHandler.go
+++ Handler/UrlHandler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"net/http"
 	"os"
 )
@@ -48,23 +49,19 @@ func (u *UserContent) GetPlaylist(p []Playlist) {
 
 func (u *UserContent) GetSpotifyPlaylist(p []Playlist) (string, error) {
 	var req *http.Request
-	var nextUrl string
-	playlistLength := 1
+	var playlistLength float64 = 1
 
-  fmt.Println(len(p))
-	for i := 0; i < playlistLength; i++ {
+	for i := 0; i < int(playlistLength); i++ {
 
-		if p[0].Total == 0 {
-			fmt.Println("1 get")
-			req, _ = http.NewRequest("GET", "https://api.spotify.com/v1/playlists/4MRFMcazMrU660XGjoyjhp/tracks?market=US&fields=items(track(name%2Calbum(name)))&offset=0", nil)
+		if i == 0 {
+			req, _ = http.NewRequest("GET", "https://api.spotify.com/v1/playlists/4MRFMcazMrU660XGjoyjhp/tracks?market=US&fields=items(track(name%2Calbum(name)))%2Cnext%2Ctotal&offset=0", nil)
 		} else {
-			fmt.Println("2 get")
-			req, _ = http.NewRequest("GET", nextUrl, nil)
+			req, _ = http.NewRequest("GET", p[i-1].Next, nil)
 		}
 
 		req.Header.Set("Accept", "application/json")
 		req.Header.Add("Content-Type", "application/json")
-		req.Header.Add("Authorization", "Bearer BQAxTgFjcWtzAL1BsRWL_Xhrgel_7XH1ScOYLXYDQPXUAA9z7DPRRpXBjRCFil7I7q0j7_-LfU5YCJwn6XwJ9-obqpPkRDjhYjewxuCfq7PKC5aggnZs-k71E-3vmutAfj90uTCY87sR5dJ5p_L8Y63JGqjAGJ3T12FU_BMAWCJB")
+		req.Header.Add("Authorization", "Bearer BQCHs9Atbz2a-r_SZYkdeWfee_54xusFhrSw1VHD5oI1cxvlE6JZDJTSEzOp5e_WD2MhtITzSdNyladK2ot59FfbvAz-Q8p0FZTsMWW1PXSKbnCFY9iZEDYWT21uXkD99OXf1S-c0M_of9c3YcuibvZp3WhDCptddzAufwI8qrHw")
 		client := &http.Client{}
 		resp, err := client.Do(req)
 		if err != nil {
@@ -80,12 +77,16 @@ func (u *UserContent) GetSpotifyPlaylist(p []Playlist) (string, error) {
 		if err != nil {
 			return "", err
 		}
-  }
-		// unmarshal not mapping correctly, need all this logic to cut p.total into a single int, then allocate
-		// a slice of the correct nice (use less memory), could instead just allocate an array like 10 which is
-		// longer then the max spotify playlist (lame)
 
-		// once array allocated, need to copy old array (playlistBackup) into new array (p)
-		// we need an else unmarshal because we dont want to reallocate everything if its allready there
+		if i == 0 {
+			playlistLength = math.Trunc(float64(p[0].Total/100)) + 1
+		}
+	}
+	// unmarshal not mapping correctly, need all this logic to cut p.total into a single int, then allocate
+	// a slice of the correct nice (use less memory), could instead just allocate an array like 10 which is
+	// longer then the max spotify playlist (lame)
+
+	// once array allocated, need to copy old array (playlistBackup) into new array (p)
+	// we need an else unmarshal because we dont want to reallocate everything if its allready there
 	return "", nil
 }
--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"Spotify-FLAC-dl/Handler"
-	"fmt"
+//	"fmt"
 )
 
 func main() {
 	h := Handler.UserContent{}
 	p := make([]Handler.Playlist, 100, 100)
 
-	var input string
-	fmt.Println("Input spotify playlist link: ")
-	fmt.Scanln(&input)
-	h.SetUrl(input)
+//	var input string
+//	fmt.Println("Input spotify playlist link: ")
+//	fmt.Scanln(&input)
+//	h.SetUrl(input)
 	h.GetSpotifyPlaylist(p)
-//  h.GetPlaylist(p)
+  h.GetPlaylist(p)
 }
