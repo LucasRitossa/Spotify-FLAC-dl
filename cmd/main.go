1f068c00a9f62f7ae728b73599e2c0005aec2c56
Lucas Ritossa <22.lucas.ritossa@westg.org>
2021-03-11 15:14:27 -0500

    link parsing

--- cmd/main.go
+++ cmd/main.go
@@ -3,23 +3,23 @@ package main
 import (
 	"Spotify-FLAC-dl/Handler"
 	"fmt"
-  "log"
+	"log"
+	"strings"
 )
 
 func main() {
 	p := make([]Handler.Playlist, 100, 100)
-  h := Handler.New(getInput())
+	h := Handler.New(getInput())
 
-  err := h.GetSpotifyPlaylist(p)
-  if err != nil {
-    log.Fatalln(err)
-  }
-  err = h.GetDeezerLinks(p)
-  if err != nil {
-    log.Fatalln(err)
-  }
-  h.DownloadAll()
-
+	err := h.GetSpotifyPlaylist(p)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = h.GetDeezerLinks(p)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	h.DownloadAll()
 }
 
 func getInput() string {
@@ -27,5 +27,8 @@ func getInput() string {
 
 	fmt.Println("Input spotify playlist link: ")
 	fmt.Scanln(&input)
-  return input 
+	input = strings.Split(input, "?si=")[0]
+	input = strings.Split(input, "playlist/")[1]
+	fmt.Println(input)
+	return input
 }
